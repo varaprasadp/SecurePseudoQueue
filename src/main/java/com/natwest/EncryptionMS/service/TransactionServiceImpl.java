6c37ee066557ac2a5afe8977b3f4391adce6fe80
varaprasadp <vara199715@gmail.com>
2022-04-09 22:51:38 +0530

    encrytption logic implemented

--- src/main/java/com/natwest/EncryptionMS/service/TransactionServiceImpl.java
+++ src/main/java/com/natwest/EncryptionMS/service/TransactionServiceImpl.java
@@ -1,5 +1,7 @@
 package com.natwest.EncryptionMS.service;
 
+import java.util.Base64;
+
 import org.springframework.stereotype.Service;
 
 import com.natwest.EncryptionMS.entity.TransactionEntity;
@@ -10,7 +12,17 @@ public class TransactionServiceImpl implements TransactionService {
 	@Override
 	public String addTransaction(TransactionEntity transaction) {
 		// TODO Auto-generated method stub
-		return transaction.toString();
+		return encryptTransaction(transaction).toString();
+	}
+	
+	public TransactionEntity encryptTransaction(TransactionEntity transaction) {
+		TransactionEntity encryptedTransaction = new TransactionEntity();
+		encryptedTransaction.setAccountNumber(Base64.getEncoder().encodeToString(transaction.getAccountNumber().getBytes()));
+		encryptedTransaction.setType(Base64.getEncoder().encodeToString(transaction.getType().getBytes()));
+		encryptedTransaction.setAmount(Base64.getEncoder().encodeToString(transaction.getAmount().getBytes()));
+		encryptedTransaction.setCurrency(Base64.getEncoder().encodeToString(transaction.getCurrency().getBytes()));
+		encryptedTransaction.setAccountFrom(Base64.getEncoder().encodeToString(transaction.getAccountFrom().getBytes()));
+		return encryptedTransaction;
 	}
 
 }
